16d8653a5851bac9b06d176135f646d0cf7bdead
JeannotMN <jeannot@ssquared.dev>
2022-04-07 15:44:58 +0200

    classes

---
+++ classes.js
@@ -0,0 +1,31 @@
+class Person {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    describe() {
+        return `Person named ${this.name}`;
+    }
+
+    static logNames(persons) {
+        for (const person of persons) {
+            console.log(person.name);
+        }
+    }
+}
+
+class Employee extends Person {
+
+    constructor(name, title) {
+        super(name);
+        this.title = title;
+    }
+
+    describe() {
+        return super.describe() + ` (${this.title})`;
+    }
+}
+
+const jane = new Employee('Jane', 'CTO');
+console.log(jane.describe());
